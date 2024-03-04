73ee02fcb3b9485f86cee77adb432652234e129f
ajagnieskus <ajagnieskus@gmail.com>
2024-03-05 00:46:27 +0200

    Ran scaffold command to create a new app

---
+++ addons/ef_library_management/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "ef_library_management",
+
+    'summary': """
+        Library management application""",
+
+    'description': """
+        Library management application
+    """,
+
+    'author': "AJagnieskus",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        #'views/views.xml',
+    ],
+}
